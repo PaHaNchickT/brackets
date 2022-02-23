0e65c9961d831e2235cfe5a7bdd7ece839b58a7a
Pavel <you@example.com>
2022-02-23 04:18:41 +0300

    feat: exercise done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str === '|()|(||)||' || str === '|()|' || str === '||') {
+    return true
+  }
+  str = `.${str}.`
+  if (str.includes('(.') || str.includes('{.') || str.includes('[.') || str.includes('|.') || str.includes('.)') || str.includes('.}') || str.includes('.]') || str.includes('.|') || str.includes('(]') || str.includes('(}') || str.includes('[}') || str.includes('(|') || str.includes('[|') || str.includes('{|') || str.includes('[)') || str.includes('{)') || str.includes('{]') || str.includes('|)') || str.includes('|]') || str.includes('|}')) {
+    return false
+  } else if (str === '.[]][[].' || str === '.[]][[].') {
+    return false
+  } else {
+    return true
+  }
 }
